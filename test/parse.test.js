03eb7b63899d603ca24eb76581583e968d6da54c
Will Johnson <william.c1.johnson@gmail.com>
2017-07-06 11:17:42 -0400

    Changing MFL to IML (#5)
    * Changing MFL to IML
    
    * - fixed eslint error
    - fixed yarn lock

--- test/parse.test.js
+++ test/parse.test.js
@@ -1,6 +1,6 @@
 // @flow
 
-import * as fp from '@mfl/fp';
+import * as fp from '@iml/fp';
 import parse from '../source/parse.js';
 import error from '../source/error.js';
 import token from '../source/token.js';
